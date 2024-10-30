86c9850adbefe3609d186946f0135b38c824a046
Bruce Ye <bruce.ye@synapxe.sg>
2024-10-30 11:52:43 +0800

    Add multitenancy support

--- DemoFhirNexusMultiTenancy/DemoFhirNexusMultiTenancy/Program.cs
+++ DemoFhirNexusMultiTenancy/DemoFhirNexusMultiTenancy/Program.cs
@@ -2,6 +2,9 @@
 // Copyright (c) Integrated Health Information Systems Pte Ltd. All rights reserved.
 // -------------------------------------------------------------------------------------------------
 
+using Ihis.FhirEngine.Core.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Reads the 'FhirEngineSerilog' configuration section to add and configure Serilog for logging audit and application logs
@@ -21,7 +24,7 @@ app.UseHsts()
     .UseEndpoints(endpoints =>
     {
         endpoints.MapFhirEngineHealthChecks("/health");
-        endpoints.MapFhirEngine();
+        endpoints.MapFhirEngine(endpoints.ServiceProvider.GetService<IOptions<FhirEngineOptions>>().Value?.PathFormat);
     });
 
 await app.RunAsync();
